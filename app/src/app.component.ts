73428cbc163080a42036a55f14cb5e8ca55f6c37
Kaustix <Kiernan.Lamarche@gmail.com>
2016-10-23 20:35:36 -0600

    Added and configured angular 2 and webpack

---
+++ app/src/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'familytree-app',
+    template: `
+    <div>Hello World!</div>
+    `
+})
+
+export class AppComponent {
+    title = 'Family Trees'
+}
